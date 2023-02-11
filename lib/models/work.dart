2852357f4f22325dfd72b8dfc60ab8a2bb6b6db0
unknown <eugeneamo85@gmail.com>
2023-02-11 02:37:43 +0000

    work model created

---
+++ lib/models/work.dart
@@ -0,0 +1,18 @@
+class Work {
+  final String name;
+  final String phone;
+  final String stylePic;
+  final String clothPic;
+  final String description;
+  final String price;
+  final String date;
+
+ const Work(
+      {required this.name,
+      required this.phone,
+      required this.stylePic,
+      required this.clothPic,
+      required this.description,
+      required this.price,
+      required this.date});
+}
