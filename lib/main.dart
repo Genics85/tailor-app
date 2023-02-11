89b5c4b80d912070d480c220b53bd6290c7eb543
unknown <eugeneamo85@gmail.com>
2023-02-11 22:55:03 +0000

    sqlite set up finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'screens/landing_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,4 +21,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
--- lib/models/work.dart
+++ lib/models/work.dart
@@ -1,18 +1,41 @@
+import 'package:sqflite/sqflite.dart';
+
 class Work {
+  final int? id;
   final String name;
   final String phone;
-  final String stylePic;
-  final String clothPic;
   final String description;
   final String price;
-  final String date;
 
- const Work(
-      {required this.name,
-      required this.phone,
-      required this.stylePic,
-      required this.clothPic,
-      required this.description,
-      required this.price,
-      required this.date});
+  const Work({
+    required this.id,
+    required this.name,
+    required this.phone,
+    required this.description,
+    required this.price,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      "_id": id,
+      "name": name,
+      "phone": phone,
+      "description": description,
+      "price": price
+    };
+  }
+
+  static Work fromJson(Map<String, Object?> json) {
+    return Work(
+        id: "_id" as int,
+        name: "name",
+        phone: "phone",
+        description: "description",
+        price: "price");
+  }
+
+  @override
+  String toString() {
+    return "Work{id:$id, name:$name, phone:$phone, description:$description, price:$price}";
+  }
 }
